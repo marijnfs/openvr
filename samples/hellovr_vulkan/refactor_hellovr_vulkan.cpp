692ee8f2af426dd045b38ad4e9ea1edffd9c47b6
Marijn Stollenga <m.stollega@gmail.com>
2017-08-29 12:45:59 +0200

    refactor

--- samples/hellovr_vulkan/refactor_hellovr_vulkan.cpp
+++ samples/hellovr_vulkan/refactor_hellovr_vulkan.cpp
@@ -39,6 +39,29 @@ void ThreadSleep( unsigned long nMilliseconds )
 #endif
 }
 
+template <typename T>
+void check(VkResult res, std::string str, T other = "") {
+  if (res != VK_SUCCESS) {
+    cerr << str << other << " error: " << res << endl;
+    throw "err";
+  }
+}
+
+void check(vr::EVRInitError err) {
+  if ( eError != vr::VRInitError_None ) {
+    cerr << "Unable to init vr: " << vr::VR_GetVRInitErrorAsEnglishDescription(err) << endl;
+    throw "";
+  }
+}
+
+void sdl_check(int err) {
+  if (err < 0) {
+    cerr << "SDL error: " << SDL_GetError() << endl;
+    throw "";
+  }
+}
+
+
 struct FencedCommandBuffer
 {
   VkCommandBuffer cmd_buffer;
@@ -92,21 +115,151 @@ struct IndexBuffer {
 struct VRSystem {
   vr::IVRSystem *hmd;
   vr::IVRRenderModels *render_models;
-  //std::string driver_str;
-  //std::string display_str;
+  std::string driver_str, display_str;
   vr::TrackedDevicePose_t tracked_pose[ vr::k_unMaxTrackedDeviceCount ];
-  Matrix4 device_pose[ vr::k_unMaxTrackedDeviceCount ];  
+  Matrix4 device_pose[ vr::k_unMaxTrackedDeviceCount ];
+
+
+  VRSystem() {
+    vr::EVRInitError err = vr::VRInitError_None;
+    hmd = vr::VR_Init( &err, vr::VRApplication_Scene );
+    check(err);
+
+    render_models = (vr::IVRRenderModels *)vr::VR_GetGenericInterface( vr::IVRRenderModels_Version, &err );
+
+    driver_str = query_str(vr::k_unTrackedDeviceIndex_Hmd, vr::Prop_TrackingSystemName_String);
+    display_str = query_str(vr::k_unTrackedDeviceIndex_Hmd, vr::Prop_SerialNumber_String);
+
+    cout << "driver: " << driver_str << " display: " << display_str << endl;
+  }
+
+  std::string query_str(vr::TrackedDeviceIndex_t devidx, vr::TrackedDeviceProperty prop) {
+      vr::TrackedPropertyError *err = NULL;
+      uint32_t buflen = hmd->GetStringTrackedDeviceProperty( devidx, prop, NULL, 0, err );
+      if( buflen == 0)
+	return "";
+
+      std::string buf(' ', buflen);
+      buflen = hmd->GetStringTrackedDeviceProperty( devidx, prop, buf.c_str(), buflen, err );
+      return buf;      
+  }
+
+  vector<string> get_ext_required() {
+    uint32_t buf_size = vr::VRCompositor()->GetVulkanInstanceExtensionsRequired( nullptr, 0 );
+    if (!buf_size)
+      return;
+
+    std::string buf(' ', buf_size);
+    vr::VRCompositor()->GetVulkanInstanceExtensionsRequired( buf.c_str(), buf_size );
+
+    // Break up the space separated list into entries on the CUtlStringList
+    vector<string> ext_list;
+    std::string cur_ext;
+    uint32_t idx = 0;
+    while ( idx < buf_size ) {
+      if ( buf[ idx ] == ' ' ) {
+	ext_list.push_back( cur_ext );
+	cur_ext.clear();
+      } else {
+	cur_ext += buf[ idx ];
+      }
+      ++idx;
+    }
+    if ( cur_ext.size() > 0 ) {
+      ext_list.push_back( cur_ext );
+    }
+
+    return ext_list;
+  }
+  
+  ~VRSystem() {
+    vr::VR_Shutdown();
+  }
 };
 
 struct WindowSystem {
   SDL_Window *window;
   uint32_t width, height;
+
+  WindowSystem() : width(800), height(800) {
+    sdl_check(SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER));
+
+    int pox_x = 700;
+    int pos_y = 100;
+    Uint32 wflags = SDL_WINDOW_SHOWN;
+
+    window = SDL_CreateWindow( "hellovr [Vulkan]", pos_x, pos_y, width, height, wflags );
+    if (!window) {
+      cerr << "SDL Window problem: " << SDL_GetError() << endl;
+      throw "";
+    }
+  }
 };
+
+
+int get_mem_type( uint32_t mem_bits, VkMemoryPropertyFlags mem_prop )
+{
+  auto vk = Global::vk();
+  for ( uint32_t i = 0; i < VK_MAX_MEMORY_TYPES; i++ )
+    {
+      if ( ( mem_bits & 1 ) == 1)
+	{
+	  // Type is available, does it match user properties?
+	  if ( ( vk.mem_prop.memoryTypes[i].propertyFlags & mem_prop ) == mem_prop )
+	    return i;
+	}
+      mem_bits >>= 1;
+    }
+
+  // No memory types matched, return failure
+  throw "err";
+}
+
 
 struct Buffer {
 	VkBuffer buffer;
 	VkDeviceMemory memory;
 
+  Buffer(size_T size, VkBufferUsageFlags usage) {
+    auto vk = Global::vk();
+    // Create the vertex buffer and fill with data
+    VkBufferCreateInfo bci = { VK_STRUCTURE_TYPE_BUFFER_CREATE_INFO };
+    bci.size = size;
+    bci.usage = usage;
+    check( vkCreateBuffer( vk.dev, &bci, nullptr, &buffer ), "vkCreateBuffer");
+    
+    VkMemoryRequirements memreq = {};
+    vkGetBufferMemoryRequirements( vk.deve, *buffer, &memreq );
+
+    VkMemoryAllocateInfo alloc_info = { VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO };
+    alloc_info.memoryTypeIndex = get_mem_type( memreq.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT );
+    alloc_info.allocationSize = memreq.size;
+
+    check( vkAllocateMemory( vk.dev, &alloc_info, nullptr, &memory ), "vkCreateBuffer" );
+    
+    check( vkBindBufferMemory( vk.dev, &buffer, &memory, 0 ), "vkBindBufferMemory" );
+
+    /*
+    if ( pBufferData != nullptr )
+      {
+		void *pData;
+		nResult = vkMapMemory( pDevice, *ppDeviceMemoryOut, 0, VK_WHOLE_SIZE, 0, &pData );
+		if ( nResult != VK_SUCCESS )
+		{
+			dprintf( "%s - vkMapMemory returned error %d\n", __FUNCTION__, nResult );
+			return false;
+		}
+		memcpy( pData, pBufferData, nSize );
+		vkUnmapMemory( pDevice, *ppDeviceMemoryOut );
+
+		VkMappedMemoryRange memoryRange = { VK_STRUCTURE_TYPE_MAPPED_MEMORY_RANGE };
+		memoryRange.memory = *ppDeviceMemoryOut;
+		memoryRange.size = VK_WHOLE_SIZE;
+		vkFlushMappedMemoryRanges( pDevice, 1, &memoryRange );
+
+	}
+	return true;
+	}*/
 };
 
 struct ViewedBuffer {
