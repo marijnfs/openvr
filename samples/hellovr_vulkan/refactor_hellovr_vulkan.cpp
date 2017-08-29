32a756a57ec47eb8457c1ce4dcd9338d2f9826f2
Marijn Stollenga <m.stollega@gmail.com>
2017-08-29 11:11:23 +0200

    changes

--- samples/hellovr_vulkan/refactor_hellovr_vulkan.cpp
+++ samples/hellovr_vulkan/refactor_hellovr_vulkan.cpp
@@ -39,29 +39,98 @@ void ThreadSleep( unsigned long nMilliseconds )
 #endif
 }
 
+struct FencedCommandBuffer
+{
+  VkCommandBuffer cmd_buffer;
+  VkFence fence;
+};
+  
 
 struct VulkanSystem {
+  VkInstance inst;
 
+  VkDevice dev;
+  VkPhysicalDevice phys_dev;
+  VkPhysicalDeviceProperties prop;
+  VkPhysicalDeviceMemoryProperties mem_prop;
+  VkPhysicalDeviceFeatures features;
+
+  VkQueue queue;
+  VkSurfaceKHR surface;
+  VkSwapchainKHR swapchain;
+
+  
+  uint32_t n_swap;
+  uint32_t frame_idx;
+  uint32_t current_frame;
+  std::vector< VkImage > swapchain_img;
+  std::vector< VkImageView > swapchain_view;
+  std::vector< VkFramebuffer > swapchain_framebuffers;
+  std::vector< VkSemaphore > swapchain_semaphores;
+  VkRenderPass m_pSwapchainRenderPass;
+
+  VkCommandPool cmd_pool;
+  VkDescriptorPool desc_pool;
+  VkDescriptorSet desc_sets[ NUM_DESCRIPTOR_SETS ];
+
+  std::deque< FencedCommandBuffer > cmd_buffers;;
+  
+  
+  VkSampler sampler;
+
+  VkDebugReportCallbackEXT debug_callback;
+
+
+};
+
+struct VertexBuffer {
+};
+
+struct IndexBuffer {
 };
 
 struct VRSystem {
-
+  vr::IVRSystem *hmd;
+  vr::IVRRenderModels *render_models;
+  //std::string driver_str;
+  //std::string display_str;
+  vr::TrackedDevicePose_t tracked_pose[ vr::k_unMaxTrackedDeviceCount ];
+  Matrix4 device_pose[ vr::k_unMaxTrackedDeviceCount ];  
 };
 
 struct WindowSystem {
-
+  SDL_Window *window;
+  uint32_t width, height;
 };
 
 struct Buffer {
 	VkBuffer buffer;
 	VkDeviceMemory memory;
 
-}
+};
 
 struct ViewedBuffer {
 	Buffer buffer;
 	VkBufferView buffer_view;
-}
+};
+
+struct Image {
+  VkImage img;
+  VkDeviceMemory mem;
+  VkImageLayout layout;
+  VkImageView view;
+
+  Image() {
+    auto vk = Global::vk();
+    
+  }
+};
+
+struct FrameBuffer {
+  Image image, depth_stencil;
+  VkRenderPass render_pass;
+  VkFramebuffer framebuffer;
+};
 
 struct Global {
 	Global() {
@@ -95,5 +164,5 @@ struct Global {
 int main() {
 	Global::inst();
 
-
+	
 }
