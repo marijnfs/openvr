b15c8ace6afa49631eef0e5cb75e1154cb4519ac
Marijn Stollenga <m.stollega@gmail.com>
2018-01-08 16:58:00 +0100

    script

--- samples/hellovr_vulkan/scene.h
+++ samples/hellovr_vulkan/scene.h
@@ -4,10 +4,33 @@
 #include "vrsystem.h"
 #include "vulkansystem.h"
 
+//triggers to set up events
+struct Trigger {
+  
+  Object *obj;
+  Controller *con;
+  
+  void check() {
+    auto pos = obj->get_pos();
+    auto con_pos = con->get_pos();
+    float d = dist(pos, con_pos);
+    if (d < .1) {
+      triggered();
+    }
+  }
+};
+
 struct Scene {
 	std::vector<GraphicsObject*> objects;
-	
+  std::vector<Trigger*> triggers;
+  
 
+  void set_rotation(vector<float> &rot);
+
+  void process_triggers(); //step through triggers
+
+  
+  
 };
 
 #endif
